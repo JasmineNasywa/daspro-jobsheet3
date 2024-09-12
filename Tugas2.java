8555b00213b86166f6bd96965dc66e48ba9069c7
JasmineNasywa <jasminenasywa6@gmail.com>
2024-09-12 13:17:00 +0700

    gaji revisi

--- Tugas2.java
+++ Tugas2.java
@@ -1,23 +1,23 @@
+
 import java.util.Scanner;
 
-Import java.util.scanner;
 public class Tugas2 {
     public static void main(String[] args) {
-    Scanner sc = new Scanner (system.in);
+    Scanner sc = new Scanner (System.in);
 
     double upahPerjam, jam;
     
     System.out.println("Masukkan upah perjam");
-    upahPerjam = sc.nextDouble()
+    upahPerjam = sc.nextDouble();
     System.out.println("Masukkan lama bekerja");
-    jam = sc.nextDouble()
+    jam = sc.nextDouble();
 
     double gajiSebulan = (upahPerjam * jam);
     double bonus = (gajiSebulan * 0.1);
     double pajak = ((gajiSebulan + bonus) * 0.05);
     double gajibersih = (gajiSebulan + bonus - pajak);
 
-    System.out.println("Gaji dan bonus satu bulan : " + gajiSebulan;
+    System.out.println("Gaji dan bonus satu bulan : " + gajiSebulan);
     System.out.println("Bonus sebulan : " + bonus);
     System.out.println("Total gaji yang didapatkan : " + gajibersih);
     }
